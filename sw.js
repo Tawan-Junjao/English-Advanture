53a35927add54bd2ecfd3ca6a524efe5c8465bb7
Tawan-Junjao <atthakornj@hotmail.com>
2026-08-05 15:33:11 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,25 +1,4 @@
-const CACHE="english-adventure-v9-monster";
-const ASSETS=[
-"./",
-"./index.html",
-"./style.css",
-"./app.js",
-"./manifest.json",
-"./icon-192.png",
-"./icon-512.png"
-];
-self.addEventListener("install",event=>{
-  event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)));
-  self.skipWaiting();
-});
-self.addEventListener("activate",event=>{
-  event.waitUntil(
-    caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key))))
-  );
-  self.clients.claim();
-});
-self.addEventListener("fetch",event=>{
-  event.respondWith(
-    caches.match(event.request).then(cached=>cached||fetch(event.request))
-  );
-});
+const C="english-adventure-v9-complete";const A=["./","./index.html","./style.css","./app.js","./manifest.json","./icon-192.png","./icon-512.png"];
+self.addEventListener("install",e=>{e.waitUntil(caches.open(C).then(c=>c.addAll(A)));self.skipWaiting()});
+self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==C).map(x=>caches.delete(x))))));
+self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
